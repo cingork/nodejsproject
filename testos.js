661160955b7dcc0dea57b8ef0b475b92eb5f7551
Gorkem Cinar <gorkem.cinar@e-kalite.com.tr>
2021-06-27 18:54:55 +0300

    #Second

--- testos.js
+++ testos.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const path =require('path');
 const express = require ('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-app.use((req,res,next)=>{
-    res.send('<h1>denemeler</h1>');
-});
+const mainRoutes = require('./routes/main');
+app.use(mainRoutes);
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(process.env.NODE_ENV || 3000);
