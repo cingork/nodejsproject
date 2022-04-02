8896130a4cb99d9118ba1977ce8f26aec8827a63
cingork <64457754+cingork@users.noreply.github.com>
2022-04-02 16:25:56 +0300

    Update testos.js

--- testos.js
+++ testos.js
@@ -8,5 +8,5 @@ app.use(mainRoutes);
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname,'/public')));
 app.listen(process.env.PORT || 3000);
